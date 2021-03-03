9c9085b998d912730eff99feafeb3310b0ea18a5
hyunjong_96 <ahajongs@gmail.com>
2021-03-03 18:07:09 +0900

    feat : 로또_구매_개수

--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -1,22 +1,11 @@
 package lotto;
 
-import spark.ModelAndView;
-import spark.template.handlebars.HandlebarsTemplateEngine;
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static spark.Spark.get;
 
 public class WebUILottoApplication {
     public static void main(String[] args) {
-        get("/", (req, res) -> {
-            Map<String, Object> model = new HashMap<>();
-            return render(model, "index.html");
-        });
+        Lotto lotto = new Lotto();
+        int numberOfLotto = lotto.lotto(1000);
+        System.out.println(numberOfLotto);
     }
 
-    private static String render(Map<String, Object> model, String templatePath) {
-        return new HandlebarsTemplateEngine().render(new ModelAndView(model, templatePath));
-    }
 }
