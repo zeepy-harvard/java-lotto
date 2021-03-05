9ecf4757a87fdeb04bd6f9d110f5e9f54b5c58c4
hyunjong_96 <ahajongs@gmail.com>
2021-03-05 17:59:40 +0900

    feat : 구입금액입력

--- src/main/java/lotto/Lotto.java
+++ src/main/java/lotto/Lotto.java
@@ -6,9 +6,10 @@ import java.util.Collections;
 import java.util.List;
 
 public class Lotto {
-    public void lottoGame(int money){
-        List<ArrayList<Integer>> lottoLists = new ArrayList();
 
+    public void lottoGame(){
+        List<ArrayList<Integer>> lottoLists = new ArrayList();
+        int money = Input.inputMoney();
         int numberOfLotto = getNumberOfLotto(money);
 
         //lottoLists = countLotto(numberOfLotto,lottoLists);
@@ -33,7 +34,6 @@ public class Lotto {
     }
 
     private int getNumberOfLotto(int money) {
-        int purchaseNumber = money/1000;
-        return purchaseNumber;
+        return money/1000;
     }
 }
--- src/main/java/lotto/WebUILottoApplication.java
+++ src/main/java/lotto/WebUILottoApplication.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class WebUILottoApplication {
     public static void main(String[] args) {
         Lotto lotto = new Lotto();
-        lotto.lottoGame(2000);
+        lotto.lottoGame();
 //        List<ArrayList<Integer>> lottoList = lotto.lotto(2000);
 //        for(int i:lottoList.get(0)){
 //            System.out.println(i);
